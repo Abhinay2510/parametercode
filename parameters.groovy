f49f713a2c12bbd391beaa13928387f41fac074f
harishpallapu <81285240+harishpallapu@users.noreply.github.com>
2023-01-06 07:30:30 +0000

    Update parameters.groovy

--- parameters.groovy
+++ parameters.groovy
@@ -1,4 +1,4 @@
-env.SAST_GIT_URL="https://github.com/harishpallapu/SourceCode_SAST.git"
-env.BUILD_GIT_URL="https://github.com/harishpallapu/SourceCode_SAST.git"
+env.SAST_GIT_URL="https://github.com/efsavage/hello-world-war.git"
+env.BUILD_GIT_URL="https://github.com/efsavage/hello-world-war.git"
 env.FUNCTIONAL_WEB_GIT_URL="https://github.com/harishpallapu/webtest.git"
 env.FUNCTIONAL_MOBILE_GIT_URL="https://github.com/harishpallapu/mobiletest.git"
