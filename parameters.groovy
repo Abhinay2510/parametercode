53460771baf84db9084db5b2c4537ae41f0def84
harishpallapu <81285240+harishpallapu@users.noreply.github.com>
2023-01-10 11:25:27 +0530

    Update parameters.groovy

--- parameters.groovy
+++ parameters.groovy
@@ -1,3 +1,4 @@
+env.BRANCH="hello"
 env.SAST_GIT_URL="https://github.com/harishpallapu/SourceCode_SAST.git"
 env.BUILD_GIT_URL="https://github.com/harishpallapu/SourceCode_SAST.git"
 env.FUNCTIONAL_WEB_GIT_URL="https://github.com/harishpallapu/webtest.git"
