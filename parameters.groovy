89e7e67972fc5c3d326536cffdf7ca7ecb66891c
harishpallapu <81285240+harishpallapu@users.noreply.github.com>
2023-01-12 10:02:57 +0000

    Update parameters.groovy

--- parameters.groovy
+++ parameters.groovy
@@ -1,6 +1,7 @@
 env.SAST_GIT_URL="https://github.com/harishpallapu/SourceCode_SAST.git"
 env.BUILD_GIT_URL="https://github.com/harishpallapu/SourceCode_SAST.git"
 env.TOMCAT_IP="http://localhost:8084/"
+env.DAST_URL=""http://140.238.240.203:8084/SpringMVCAnnotationShoppingCart/
 env.FUNCTIONAL_WEB_GIT_URL=""
 env.FUNCTIONAL_MOBILE_GIT_URL=""
-env.JMETER_GIT_URL=""
+env.JMETER_GIT_URL="https://github.com/harishpallapu/folder.git"
