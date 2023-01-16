ca1586c7849d511dfb3519817cc5ef74ecf60be5
harishpallapu <81285240+harishpallapu@users.noreply.github.com>
2023-01-16 06:02:14 +0000

    Update parameters.groovy

--- parameters.groovy
+++ parameters.groovy
@@ -1,6 +1,6 @@
 env.SAST_GIT_URL="https://github.com/harishpallapu/SourceCode_SAST.git"
 env.BUILD_GIT_URL="https://github.com/harishpallapu/SourceCode_SAST.git"
-env.Deployment="http://140.238.240.203:8084/"
+env.Deployment="http://localhost:8084/"
 env.DAST_URL="https://hub.docker.com"
 env.FUNCTIONAL_WEB_GIT_URL=""
 env.FUNCTIONAL_MOBILE_GIT_URL=""
