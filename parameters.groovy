743939bf51e6142d4a8621cf85da3ec0193b63eb
harishpallapu <81285240+harishpallapu@users.noreply.github.com>
2023-01-17 05:22:27 +0000

    Update parameters.groovy

--- parameters.groovy
+++ parameters.groovy
@@ -1,6 +1,7 @@
 env.SAST_GIT_URL="https://github.com/harishpallapu/SourceCode_SAST.git"
 env.BUILD_GIT_URL="https://github.com/harishpallapu/SourceCode_SAST.git"
 env.Deployment="http://localhost:8084/"
+env.WAR_FILE="SpringMVCAnnotationShoppingCart.war"
 env.DAST_URL="http://140.238.240.203:8084/SpringMVCAnnotationShoppingCart"
 env.FUNCTIONAL_WEB_GIT_URL="https://github.com/harishpallapu/webtest.git"
 env.FUNCTIONAL_MOBILE_GIT_URL="https://github.com/harishpallapu/mobiletest.git"
